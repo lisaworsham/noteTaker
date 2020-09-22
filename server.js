5641624349f6aa2729f72262a77ced3d37f54e2a
Lisa Worsham <lisa.r.hyndman@gmail.com>
2020-09-22 14:17:27 -0500

    setup html routes

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
