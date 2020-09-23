df568dc299860acc6824aa2f50fa17e08d4283c1
Lisa Worsham <lisa.r.hyndman@gmail.com>
2020-09-22 22:11:24 -0500

    created apiroutes

--- routes/apiRoutesredo.js
+++ routes/apiRoutesredo.js
@@ -3,36 +3,31 @@ const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = function(app) {
-
     app.get("/api/notes", function(req, res) {
         res.json(db);
     });
-  
+
     app.post("/api/notes", function(req, res) {
         let id = {id: uuidv4()}
         const newNote = Object.assign(req.body, id);
         db.push(newNote);
-        fs.writeFileSync("./db/db.json", JSON.stringify(db,null,1));
+        fs.writeFileSync("./db.db.json", JSON.stringify(db,null,1));
         res.json(db);
     });
-  
+
     app.delete("/api/notes/:id", function(req, res) {
         const noteId = req.params.id
         console.log(noteId)
-        for (i = 0; i < db.length; i++){
+        for (let i = 0; i < db.length; i++){
+            console.log("for loop works")
             const note = db[i];
-            if (noteId.indexOf(note) !== -1){
+            if (noteId === note.id){
+                console.log("we reached here")
                 db.splice(i, 1);
                 i--;
                 fs.writeFileSync("./db/db.json", JSON.stringify(db,null,1), "utf-8");
             }
         }
-        return res.json(db)
-        
-        //for loop to loop through db.json file.
-        //then grab the note object from that index
-        //validate if the note id is = to the id of the note object you pulled, you are going to *splice* that out
-        //after you splice, you are going to write your file to the db.json
-        //return res.json(db)
+        return res.json(db);
     })
 }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutesredo")(app);
 require("./routes/htmlRoutes")(app);
 
 
